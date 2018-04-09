6eb68874cfa7e38ae41dff9d67f945e11bd30d6f
Kevin Baker <kbaker@e1z4r6p19.42.us.org>
2018-04-09 16:44:39 -0700

    hereyagolarryson

--- fillit.h
+++ fillit.h
@@ -6,7 +6,7 @@
 /*   By: lapang <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/23 22:28:20 by lapang            #+#    #+#             */
-/*   Updated: 2018/03/27 21:01:32 by lapang           ###   ########.fr       */
+/*   Updated: 2018/04/02 12:50:54 by kbaker           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,22 @@
 # define FILLIT_H
 
 # include "./libft/libft.h"
+# include <sys/stat.h>
+# include <fcntl.h>
 
 typedef	struct	s_map
 {
-	int			size;
+	int			xmax;
+	int			ymax;
 	char		**array;
 }				t_map;
+
+typedef struct s_char
+{
+	int			hash;
+	int			dot;
+	int			newline;
+}				t_char;
+
 
 #endif
--- main.c
+++ main.c
@@ -3,55 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lapang <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: kbaker <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/03/23 21:42:57 by lapang            #+#    #+#             */
-/*   Updated: 2018/03/27 22:23:29 by lapang           ###   ########.fr       */
+/*   Created: 2018/03/31 14:41:12 by kbaker            #+#    #+#             */
+/*   Updated: 2018/03/31 14:49:00 by kbaker           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "fillit.h"
-
-#define BUF_SIZE 4096
-
-void	ft_putchar(char c)
+int		main(int argc, char **argv)
 {
-	write(1, &c, 1);
-}
+	char	*tetremino;
+	char	**solving;
 
-void	ft_putstr(char const *s)
-{
-	int i;
-
-	i = 0;
-	if (!s)
-		return ;
-	while (s[i] != '\0')
+	if (argc != 2)
 	{
-		ft_putchar(s[i]);
-		i++;
+		ft_putstr("usage: fillit input_file\n");
+		return (0);
 	}
-}
-
-int	main(int argc, char **argv)
-{
-	int		fd;
-	int		ret;
-	char	buf[BUF_SIZE];
-
-	if (argc == 2)
-	{
-		fd = open(argv[1], O_RDONLY);
-		if (fd == -1)
-		{
-			ft_putstr("usage: ./fillit valid_tetrimino_input\n");
-			return (1);
-		}
-		ret = read(fd, buf, BUF_SIZE);
-		buf[ret] = '\0';
-		ft_putstr(buf);
-	}
-	else
-		ft_putchar('\n');
+	tetremino = ft_get_tetremino(argv[i]);
+	solving = fillit(tetremino);
+	ft_print_array(solution);
 	return (0);
 }

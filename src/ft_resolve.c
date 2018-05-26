41b3c82be25b21f23308718108bced4904fec356
Kevin Baker <kbaker@e1z2r4p26.42.us.org>
2018-05-25 23:26:19 -0700

    stuff2

---
+++ src/ft_resolve.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_resolve.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kbaker <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/05/25 22:43:20 by kbaker            #+#    #+#             */
+/*   Updated: 2018/05/25 22:43:21 by kbaker           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fillit.h"
+
+int		ft_test_piece(char **grid, int x, int y, t_piece piece)
+{
+	if (grid[piece.b1.y + y][piece.b1.x + x] == '.' &&
+		grid[piece.b2.y + y][piece.b2.x + x] == '.' &&
+		grid[piece.b3.y + y][piece.b3.x + x] == '.' &&
+		grid[piece.b4.y + y][piece.b4.x + x] == '.')
+		return (1);
+	else
+		return (0);
+}
+
+int		ft_test_coord(int size, int x, int y, t_piece piece)
+{
+	if ((piece.b1.y + y) < size && (piece.b1.x + x) < size &&
+		(piece.b2.y + y) < size && (piece.b2.x + x) < size &&
+		(piece.b3.y + y) < size && (piece.b3.x + x) < size &&
+		(piece.b4.y + y) < size && (piece.b4.x + x) < size)
+		return (1);
+	else
+		return (0);
+}
+
+void	ft_put_piece(char **grid, int xy[2], t_piece piece, char letter)
+{
+	grid[piece.b1.y + xy[1]][piece.b1.x + xy[0]] = letter;
+	grid[piece.b2.y + xy[1]][piece.b2.x + xy[0]] = letter;
+	grid[piece.b3.y + xy[1]][piece.b3.x + xy[0]] = letter;
+	grid[piece.b4.y + xy[1]][piece.b4.x + xy[0]] = letter;
+}
+
+void	ft_rm_piece(char **grid, int x, int y, t_piece piece)
+{
+	grid[piece.b1.y + y][piece.b1.x + x] = '.';
+	grid[piece.b2.y + y][piece.b2.x + x] = '.';
+	grid[piece.b3.y + y][piece.b3.x + x] = '.';
+	grid[piece.b4.y + y][piece.b4.x + x] = '.';
+}
+
+int		ft_resolve(char **grid, char let, t_piece *piece, int nb_p)
+{
+	int		xy[3];
+
+	xy[2] = ft_get_gridsize(grid);
+	xy[1] = 0;
+	while (xy[1] < xy[2])
+	{
+		xy[0] = 0;
+		while (xy[0] < xy[2])
+		{
+			if (ft_test_coord(xy[2], xy[0], xy[1], *piece) &&
+				ft_test_piece(grid, xy[0], xy[1], *piece))
+			{
+				ft_put_piece(grid, xy, *piece, let);
+				if (piece->id == nb_p)
+					return (1);
+				if (ft_resolve(grid, let + 1, piece + 1, nb_p))
+					return (1);
+				else
+					ft_rm_piece(grid, xy[0], xy[1], *piece);
+			}
+			xy[0] += 1;
+		}
+		xy[1] += 1;
+	}
+	return (0);
+}
---
+++ src/ft_stock_pieces.c
@@ -0,0 +1,93 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_stock_pieces.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kbaker <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/05/25 22:43:00 by kbaker            #+#    #+#             */
+/*   Updated: 2018/05/25 22:43:01 by kbaker           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fillit.h"
+
+char		**ft_get_tetri2(void)
+{
+	char	**tetri;
+
+	if (!(tetri = (char**)malloc(sizeof(char*) * 19)))
+		return (NULL);
+	tetri[0] = "00102021";
+	tetri[1] = "10111202";
+	tetri[2] = "00011121";
+	tetri[3] = "00100102";
+	tetri[4] = "00102011";
+	tetri[5] = "10011112";
+	tetri[6] = "10011121";
+	tetri[7] = "00011102";
+	tetri[8] = "00102001";
+	tetri[9] = "00101112";
+	tetri[10] = "20011121";
+	tetri[11] = "00010212";
+	tetri[12] = "10200111";
+	tetri[13] = "00011112";
+	tetri[14] = "00101121";
+	tetri[15] = "10011102";
+	tetri[16] = "00010203";
+	tetri[17] = "00102030";
+	tetri[18] = "00100111";
+	return (tetri);
+}
+
+int			ft_count_pieces(char *str_pieces)
+{
+	return ((ft_strlen(str_pieces) + 1) / 21);
+}
+
+void		ft_print_grid(char **tab)
+{
+	int i;
+	int	nb;
+
+	i = 0;
+	nb = ft_get_gridsize(tab);
+	while (i < nb)
+		ft_putendl(tab[i++]);
+}
+
+static int	ft_ctoi(char c)
+{
+	if (c >= '0' && c <= '9')
+		c -= 48;
+	return (c);
+}
+
+t_piece		*ft_stock_pieces(char *str_pieces, int nb_pieces)
+{
+	int		i;
+	t_piece	*tab;
+	t_piece	piece;
+	char	**tetri;
+	int		nb;
+
+	i = 0;
+	nb = 1;
+	tetri = ft_get_tetri2();
+	tab = (t_piece*)malloc(sizeof(t_piece) * nb_pieces);
+	while (i < nb_pieces)
+	{
+		piece.id = nb++;
+		piece.b1.x = ft_ctoi(tetri[ft_get_pattern(str_pieces)][0]);
+		piece.b1.y = ft_ctoi(tetri[ft_get_pattern(str_pieces)][1]);
+		piece.b2.x = ft_ctoi(tetri[ft_get_pattern(str_pieces)][2]);
+		piece.b2.y = ft_ctoi(tetri[ft_get_pattern(str_pieces)][3]);
+		piece.b3.x = ft_ctoi(tetri[ft_get_pattern(str_pieces)][4]);
+		piece.b3.y = ft_ctoi(tetri[ft_get_pattern(str_pieces)][5]);
+		piece.b4.x = ft_ctoi(tetri[ft_get_pattern(str_pieces)][6]);
+		piece.b4.y = ft_ctoi(tetri[ft_get_pattern(str_pieces)][7]);
+		tab[i++] = piece;
+		str_pieces += 21;
+	}
+	return (tab);
+}
